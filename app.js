4d3dbc419524f0f7db613286e247bd3172b78c32
alexandrebis <alex.bis8@hotmail.fr>
2019-10-16 17:01:05 +0200

    change name of database

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI+"/dbStar",
+mongoose.connect(process.env.MONGO_URI+"/bdStar",
     {useNewUrlParser: true, useCreateIndex: true})
     .then(() => {
       console.log("Connected to database!");
