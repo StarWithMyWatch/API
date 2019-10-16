61352c092313b55f5cb531ba75d053b48d82bfb0
alexandrebis <alex.bis8@hotmail.fr>
2019-10-16 14:46:10 +0200

    first commit for API base

---
+++ app.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const montresRoutes = require("./routes/montre");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bdStar',
+    {useNewUrlParser: true, useCreateIndex: true})
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch(() => {
+      console.log("Connection failed!");
+    });
+var db = mongoose.connection;
+
+db.collection("nodes").countDocuments(
+    {}, // filters
+    {}, // options
+    function(error, result) {
+      console.log(result);
+    }
+);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+app.use("/api/montres", montresRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
